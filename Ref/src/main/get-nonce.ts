0fded8990677559123bd63c62c1eaa5d51b8be07
yzchen14 <66830862+yzchen14@users.noreply.github.com>
2025-09-19 19:04:40 +0800

    OK

--- Ref/src/main/get-nonce.ts
+++
@@ -1,9 +0,0 @@
-/* Copyright 2021, Milkdown by Mirone.*/
-export function getNonce() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
